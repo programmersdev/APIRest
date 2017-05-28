37e4d0479b1c95448cac1c34a3d6860a67e7b332
Victor Cortés <victor.aljamar@gmail.com>
2017-05-28 17:03:07 +0200

    bd connection

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2test',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=127.0.0.1:3307;dbname=apidb',
+    'username' => 'programmersdev',
+    'password' => 'IESMajuelo2017db',
     'charset' => 'utf8',
 ];
